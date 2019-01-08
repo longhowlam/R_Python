78837315e3b8505fb7de5a7864bf4999efaccddf
longhowlam <longhowlam@gmail.com>
2019-01-08 12:15:47 +0100

    code

---
+++ dataprep.py
@@ -0,0 +1,25 @@
+################################################
+##
+## basic data prep with pandas
+
+import pandas as pd
+
+## import data sets
+huis = pd.read_csv("huizen.csv")
+PC = pd.read_csv("postcodes.csv")
+
+## join on postcodes
+huis2 = huis.merge(
+    PC,
+    how = 'left', 
+    left_on = 'PC6', 
+    right_on = 'Postcode_spatie'
+)
+
+## gemiddkde prijs per provincie
+Prov = huis2.\
+    groupby('province_code')['prijs'].\
+    agg(['min', 'max', 'mean', 'median']).\
+    sort_values(['mean'])
+
+Prov
--- introductie.R
+++ introductie.R
@@ -1,5 +1,27 @@
-x <- 9
-ls()
+##############################################
+##
+## basic data prep with tidyverse
 
-x = 9
+library(tidyverse)
 
+huis = read_csv("huizen.csv")
+PC = read_csv("postcodes.csv")
+
+## join on postcodes
+huis2 = huis %>% 
+    left_join(
+        PC, 
+        by = c("PC6"="Postcode_spatie")
+    )
+
+## gemiddelde prijs per provincie
+Prov = huis2 %>%
+    group_by(province_code) %>%
+    summarise(
+        min  = min(prijs),
+        max  = max(prijs),
+        mean = mean(prijs)
+    ) %>%
+    arrange(mean)
+
+Prov
