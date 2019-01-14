3afbb7d4051a1229043d139afcf6a475d93cd90e
longhowlam <longhowlam@gmail.com>
2019-01-14 09:20:45 +0100

    code

--- machinelearning.py
+++ machinelearning.py
@@ -80,3 +80,100 @@ r2_score(y,y_pred)
 ##
 ## classification model
 
+import pandas as pd
+
+## import titanic data set
+titanic = pd.read_csv("titanic.csv")
+titanic.shape
+
+# define decision tree object from scikit learn
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier(
+    random_state = 1337,
+    criterion = 'gini',
+    splitter = 'best',
+    max_depth = 5,
+    min_samples_leaf = 10
+)
+
+# Features for which we drop rows with missing values"
+drop_rows_when_missing = ['Age', 'Pclass']
+for feature in drop_rows_when_missing:
+    titanic = titanic[titanic[feature].notnull()]
+
+titanic.shape
+
+# dummie encoding # Only keep the top 100 values
+LIMIT_DUMMIES = 100
+from collections import defaultdict, Counter
+
+categorical_to_dummy_encode = ['Pclass', 'Sex']
+
+def select_dummy_values(train, features):
+    dummy_values = {}
+    for feature in categorical_to_dummy_encode:
+        values = [
+            value
+            for (value, _) in Counter(train[feature]).most_common(LIMIT_DUMMIES)
+        ]
+        dummy_values[feature] = values
+    return dummy_values
+
+DUMMY_VALUES = select_dummy_values(titanic, categorical_to_dummy_encode)
+
+def dummy_encode_dataframe(df):
+    for (feature, dummy_values) in DUMMY_VALUES.items():
+        for dummy_value in dummy_values:
+            dummy_name = u'%s_value_%s' % (feature, dummy_value)
+            df[dummy_name] = (df[feature] == dummy_value).astype(float)
+        del df[feature]
+
+dummy_encode_dataframe(titanic)
+
+# show the created dummy columns
+titanic[['Pclass_value_1', 'Pclass_value_2', 'Pclass_value_3', 'Sex_value_male', 'Sex_value_female']]
+
+# set X and Y, the input features and the target
+X = titanic.iloc[:,[3,10,11,12,13,14]] 
+Y = titanic['Survived']
+
+# train the decison tree
+clf.fit(X, Y)
+
+# plot the tree
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("titanic") 
+
+dot_data = tree.export_graphviz(
+    clf, out_file=None, 
+    feature_names = list(X),  
+    class_names = ['Y','N'],  
+    filled=True, rounded=True,  
+    special_characters=True
+)  
+graph = graphviz.Source(dot_data)  
+graph.render("titanic") 
+
+#### area under curve metric
+# comparison of predicted scores and the true label
+pred_scores = clf.predict_proba(X)
+
+# The magic happens in the scikit-plot package.....
+import matplotlib.pyplot as plt
+import scikitplot as skplt
+skplt.metrics.plot_roc(titanic.Survived, pred_scores)
+plt.show()
+
+
+
+
+
+
+
+
+
+
